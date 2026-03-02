fea2b07f7180b96b5c3e1df45ff6f4c53dd297f7
rabi-minhnhut <nhut@rabi.co.jp>
2026-03-02 18:02:59 +0700

    a

--- src/app/(index)/components/HomeNews.tsx
+++ src/app/(index)/components/HomeNews.tsx
@@ -165,7 +165,7 @@ const HomeNews = async () => {
           </div>
           <div className="mt-12 flex justify-center md:mt-[54px]">
             <Link
-              href="/"
+              href="/news"
               className="flex h-14 w-[260px] items-center justify-center gap-5 bg-[#FF4E4E] text-[16px] font-bold text-white md:h-[70px] md:w-[300px] md:text-[20px]"
             >
               ニュース一覧
