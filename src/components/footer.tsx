eaa034bdcfcba6167ac5bfd569990c7403a132b2
rabi-minhnhut <nhut@rabi.co.jp>
2026-03-24 21:08:57 +0700

    a

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,10 +13,10 @@ const Footer = () => {
         </div>
         <ul className="flex items-center justify-center gap-6 pt-12.5 pb-20 text-[16px] max-md:flex-col md:gap-25 md:pb-25 md:text-[18px]">
           <li>
-            <Link href="/">利用規約</Link>
+            <Link href="/terms">利用規約</Link>
           </li>
           <li>
-            <Link href="/">プライバシーポリシー</Link>
+            <Link href="/privacy-policy">プライバシーポリシー</Link>
           </li>
           <li>
             <Link href="/">運営会社</Link>
